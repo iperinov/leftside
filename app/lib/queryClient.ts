76074c886cae8fe73a0a68500ab9afd90987a804
Ivo Perinov <59851417+iperinov@users.noreply.github.com>
2025-07-03 17:03:53 +0300

    Added infinite caching confir for React Query

--- app/lib/queryClient.ts
+++ app/lib/queryClient.ts
@@ -1,3 +1,22 @@
 import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // Disable refetching on window focus
+      //refetchOnWindowFocus: false,
+
+      // Disable refetching on mount or reconnect
+      //refetchOnMount: false,
+      //refetchOnReconnect: false,
+
+      // Never expire 
+      staleTime: Infinity,
+
+      // Cache forever
+      gcTime: Infinity,
+    },
+  },
+});
+
+
